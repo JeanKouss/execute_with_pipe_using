246d427a86b6dc981594aa7c0747ff8a1c116b03
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-12-25 19:01:33 +0000

    Using ping as demo process

--- count.py
+++
@@ -1,9 +0,0 @@
-print("Let's count")
-
-for i in range(5) :
-    print(i)
-
-input("Give an input to restart")
-
-for i in range(5) :
-    print(i)
--- main_thread_reading/MainThreadReading.gd
+++ main_thread_reading/MainThreadReading.gd
@@ -1,23 +1,12 @@
 extends Control
 
-var prime_program_path = "count.py"
 var process_io : FileAccess
-var prime_process = null
-var read_next_line : bool = false
+var process = null
 
 func _ready():
-	prime_process = OS.execute_with_pipe("python3", [prime_program_path])
-	process_io = prime_process['stdio']
-	pass
+	process = OS.execute_with_pipe("cmd.exe", ['/c', 'ping 8.8.8.8 -t'])
+	process_io = process['stdio']
+
 
 func _process(_delta: float) -> void:
-	if read_next_line :
-		print(process_io.get_line())
-		read_next_line = false
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_released("ui_accept") :
-		if process_io.is_open() and process_io.get_error() == OK :
-			read_next_line = true
-			
-			
+	print(process_io.get_line())
